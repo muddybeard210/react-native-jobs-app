4edc8b4c01b1480773c992c99142ef6e8b861a8c
Matt <msmith@scisolutions.com>
2018-08-29 10:23:51 -0700

    added basic header text for reviewscreen

--- screens/ReviewScreen.js
+++ screens/ReviewScreen.js
@@ -3,6 +3,10 @@ import { View, Text } from "react-native";
 
 class ReviewScreen extends Component {
   state = {};
+  //'navigationOptions' is a very specific name used for react navigation to pull out route specific customizations.  If you name this prop something else, it will not know to pull this data
+  static navigationOptions = {
+    title: "Review Jobs"
+  };
   render() {
     return (
       <View>
