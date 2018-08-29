2e0df3fa7355dbea8a208dc2e8db3d88f00bfa00
Matt <msmith@scisolutions.com>
2018-08-29 12:36:10 -0700

    added some styling updates to the slides component and updated the data object to include a backgroundColor prop

--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -2,10 +2,16 @@ import React, { Component } from "react";
 import { View, Text } from "react-native";
 import Slides from "../components/Slides";
 
+const SLIDE_DATA = [
+  { text: "Welcome to JobApp", backgroundColor: "#03A9F4" },
+  { text: "Use this app to find your next job!", backgroundColor: "#009688" },
+  { text: "Set your location, then swipe away", backgroundColor: "#03A9F4" }
+];
+
 class WelcomeScreen extends Component {
   state = {};
   render() {
-    return <Slides />;
+    return <Slides data={SLIDE_DATA} />;
   }
 }
 
