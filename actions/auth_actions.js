9e883c8542f67cde10c5825eac5687c59cad2608
muddybeard210 <mattsmith203@hotmail.com>
2018-09-15 08:58:11 -0700

    facebook modal now showing and saving token to asynstorage

--- actions/auth_actions.js
+++ actions/auth_actions.js
@@ -1,6 +1,6 @@
 import { AsyncStorage } from "react-native";
 import { Facebook } from "expo";
-import { FACEBOOK_LOGIN_SUCCESS } from "./types";
+import { FACEBOOK_LOGIN_SUCCESS, FACEBOOK_LOGIN_FAIL } from "./types";
 
 //Storage works like local storage but its async so it returns a promise...
 //AsyncStorage.setItem("fb_token", token);
@@ -15,16 +15,29 @@ export const facebookLogin = () => async dispatch => {
       payload: token
     });
   } else {
-    doFacebookLogin();
     //Start up FB login process
+    doFacebookLogin(dispatch);
   }
 };
 
-const doFacebookLogin = async () => {
-  let result = await Facebook.logInWithReadPermissionsAsync(
+const doFacebookLogin = async dispatch => {
+  let { type, token } = await Facebook.logInWithReadPermissionsAsync(
     "1944684085595530",
     {
       permissions: ["public_profile"]
     }
   );
+  // anytime a user either cancels the login or enters in wrong information, type will equal 'cancel' so we can use that info to do logic off of the type
+  if (type === "cancel") {
+    return dispatch({
+      type: FACEBOOK_LOGIN_FAIL
+    });
+  }
+
+  //The user has logged in at this point
+  await AsyncStorage.setItem("fb_token", token);
+  dispatch({
+    type: FACEBOOK_LOGIN_SUCCESS,
+    payload: token
+  });
 };
